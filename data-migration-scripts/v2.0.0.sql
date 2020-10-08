351de79f4385bddc2e0b414bb6cefdb873667f19
Mateusz Żółtak <zozlak@zozlak.org>
2020-10-08 19:18:35 +0200

    Data check & migration script for ontology v2.0.0 added

---
+++ data-migration-scripts/v2.0.0.sql
@@ -0,0 +1,74 @@
+BEGIN;
+
+--------------------------------------------------------------------------------
+-- object/datatype property mismatch
+--------------------------------------------------------------------------------
+select m2.* from metadata m1 join identifiers i using (id) join metadata m2 on i.ids = m2.property where m1.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' and m1.value = 'http://www.w3.org/2002/07/owl#ObjectProperty';
+select m2.* from metadata m1 join identifiers i using (id) join relations m2 on i.ids = m2.property where m1.property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' and m1.value = 'http://www.w3.org/2002/07/owl#DatatypeProperty';
+
+--------------------------------------------------------------------------------
+-- removed inverse properties
+--------------------------------------------------------------------------------
+select * from relations where property in ('https://vocabs.acdh.oeaw.ac.at/schema#isContinuedBy', 'https://vocabs.acdh.oeaw.ac.at/schema#isDocumentedBy', 'https://vocabs.acdh.oeaw.ac.at/schema#isActorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isAuthorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isContactOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isContributorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isCoverageOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isCreatorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isCuratorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isDepositorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isDerivedPublicationOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isEditorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isFunderOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isHostingOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isLicensorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#hasMember', 'https://vocabs.acdh.oeaw.ac.at/schema#hasMetadata', 'https://vocabs.acdh.oeaw.ac.at/schema#isMetadataCreatorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isOwnerOf', 'https://vocabs.acdh.oeaw.ac.at/schema#hasPart', 'https://vocabs.acdh.oeaw.ac.at/schema#isPrincipalInvestigatorOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isRightsHolderOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isSourceOf', 'https://vocabs.acdh.oeaw.ac.at/schema#isSpatialCoverageOf', 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitleImage', 'https://vocabs.acdh.oeaw.ac.at/schema#isPreviousVersionOf');
+
+--------------------------------------------------------------------------------
+-- properties in the acdh namespace not mentioned in the schema
+--------------------------------------------------------------------------------
+select *
+    from (select property, count(*) from metadata where property like 'https://vocabs.acdh.oeaw.ac.at%' group by 1) t
+    where not exists (select 1 from identifiers where t.property = ids);
+select *
+    from (select property, count(*) from relations where property like 'https://vocabs.acdh.oeaw.ac.at%' group by 1) t
+    where not exists (select 1 from identifiers where t.property = ids);
+
+--------------------------------------------------------------------------------
+-- compute hasAccessRestrictionSummary and hasLicenseSummary for collections
+--------------------------------------------------------------------------------
+CREATE TEMPORARY TABLE _collections AS
+    SELECT id FROM metadata WHERE property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' AND substring(value, 1, 1000) = 'https://vocabs.acdh.oeaw.ac.at/schema#Collection';
+CREATE TEMPORARY TABLE _children AS
+    SELECT id AS cid, (get_relatives(id, 'https://vocabs.acdh.oeaw.ac.at/schema#isPartOf', 0)).* FROM _collections;
+CREATE TEMPORARY TABLE _aggregates AS
+    SELECT 
+      id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasAccessRestrictionSummary' AS property, lang, 
+      string_agg(value || ' ' || count, E'\\n' ORDER BY count DESC, value) AS value
+    FROM (
+        SELECT cid AS id, rl.property, lang, value, count(*) AS count
+        FROM
+            _children r
+            JOIN relations rl ON r.id = rl.id AND rl.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasAccessRestriction'
+            JOIN metadata m ON rl.target_id = m.id AND m.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle'
+        GROUP BY 1, 2, 3, 4
+    ) a1
+    GROUP BY 1, 2, 3;
+INSERT INTO _aggregates
+    SELECT 
+      id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasLicenseSummary' AS property, lang, 
+      string_agg(value || ' ' || count, E'\\n' ORDER BY count DESC, value) AS value
+    FROM (
+        SELECT cid AS id, rl.property, lang, value, count(*) AS count
+        FROM
+            _children r
+            JOIN relations rl ON r.id = rl.id AND rl.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasLicense'
+            JOIN metadata m ON rl.target_id = m.id AND m.property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasTitle'
+        GROUP BY 1, 2, 3, 4
+    ) a1
+    GROUP BY 1, 2, 3;
+-- for empty collections
+INSERT INTO _aggregates 
+    SELECT id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasAccessRestrictionSummary', 'en', '' 
+    FROM _collections c 
+    WHERE NOT EXISTS (SELECT 1 FROM _aggregates WHERE c.id = id);
+INSERT INTO _aggregates 
+    SELECT id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasLicenseSummary', 'en', '' 
+    FROM _collections c 
+    WHERE NOT EXISTS (SELECT 1 FROM _aggregates WHERE c.id = id);
+-- insert
+INSERT INTO metadata (mid, id, property, type, lang, value)
+    SELECT nextval('mid_seq'), id, property, 'http://www.w3.org/2001/XMLSchema#string', lang, value FROM _aggregates;
+
+
+--------------------------------------------------------------------------------
+-- commit changes
+--------------------------------------------------------------------------------
+COMMIT;
